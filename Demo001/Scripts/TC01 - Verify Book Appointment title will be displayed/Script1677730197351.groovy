323a299536afcc636feec383c061c379a897d1ea
Spider <darknessnd.252@gmail.com>
2023-03-13 15:16:49 +0700

    remove library, add comment

--- Demo001/Scripts/TC01 - Verify Book Appointment title will be displayed/Script1677730197351.groovy
+++ Demo001/Scripts/TC01 - Verify Book Appointment title will be displayed/Script1677730197351.groovy
@@ -1,30 +1,24 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import internal.GlobalVariable as GlobalVariable
 
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
+//Open browser
 WebUI.openBrowser('https://katalon-demo-cura.herokuapp.com/profile.php#login')
 
 WebUI.maximizeWindow()
 
+//Input username
 WebUI.setText(findTestObject('Login_TC01/txt_username'), 'John Doe')
 
+//Input Password
 WebUI.setText(findTestObject('Login_TC01/txt_password'), 'ThisIsNotAPassword')
 
+//Login
 WebUI.click(findTestObject('Login_TC01/btn_login'))
 
 WebUI.waitForPageLoad(30)
 
+//verify 'Book Appointment' available 
 WebUI.verifyTextPresent('Book Appointment', false)
 
 WebUI.closeBrowser()
--- Demo001/Scripts/TC02 - Verify the Welcome message will be displayed/Script1677554087400.groovy
+++ Demo001/Scripts/TC02 - Verify the Welcome message will be displayed/Script1677554087400.groovy
@@ -1,32 +1,24 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import internal.GlobalVariable as GlobalVariable
 
 WebUI.openBrowser('https://the-internet.herokuapp.com/login')
 
 WebUI.maximizeWindow()
 
+//Input username
 WebUI.setText(findTestObject('Login_TC02/txt_username2'), 'tomsmith')
 
+//Input Password
 WebUI.setText(findTestObject('Login_TC02/txt_password2'), 'SuperSecretPassword!')
 
+//Login
 WebUI.click(findTestObject('Login_TC02/btn_login2'))
 
 WebUI.waitForPageLoad(30)
 
+//Verify text 'Welcome to the Secure Area. When you are done click logout below' available
 WebUI.verifyTextPresent('Welcome to the Secure Area. When you are done click logout below', false)
-
 
 WebUI.closeBrowser()
 
